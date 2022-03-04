53c57e9e8b20f553d31fe418d2ca5cead9fecf76
Michael Bestas <mkbestas@gmail.com>
2022-03-04 18:58:19 +0200

    Override PLATFORM_SECURITY_PATCH
    * The current stock update we are based on has a security patch level of 2022-02-01,
      which we need to report for SafetyNet to pass. The user will still see the real
      security patch level in Settings with I18f8b6812335f132a935e0cfc04523cf693d1101
    
    Change-Id: I64af3687ff5ceeff0f12e9ab8f47db84f8ec8a70

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -171,6 +171,7 @@ CUSTOM_APNS_FILE := $(COMMON_PATH)/configs/apns-conf.xml
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
+PLATFORM_SECURITY_PATCH := 2022-02-01
 VENDOR_SECURITY_PATCH := 2022-02-01
 
 # Sepolicy
